1773f59f985be4be41814b3a141ab206d45c9dc0
Husni Kamal <husnikamal3@gmail.com>
2024-07-14 10:12:59 +0700

    consuming blog api

---
+++ apps/next-blog-page-router/model/blog.ts
@@ -0,0 +1,10 @@
+export type Blog = {
+  id?: number;
+  title?: string;
+  content?: string;
+  metaTitle?: string;
+  metaDescription?: string;
+  slug?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
